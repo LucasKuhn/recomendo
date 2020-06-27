8520579d3d37f745835923fb7bf4dbc651642e5f
Lucas Kuhn <lucas@nordweg.com>
2020-06-27 18:43:09 -0300

    cleanup user agent

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -10,23 +10,24 @@ class CorsController < ApplicationController
     url = params[:url]
     # https://github.com/jhy/jsoup/issues/976
     # https://medium.com/@tusharseth93/scraping-the-web-a-fast-and-simple-way-to-scrape-amazon-b3d6d74d649f
-    document = Nokogiri::HTML(open(url, "User-Agent" => "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"))
+    user_agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    document = Nokogiri::HTML(open(url, "User-Agent" => user_agent))
 
     open_graph = document.css('meta[property^="og"]')
-    
+
     if open_graph.present?
       result = open_graph.map { |elm| [elm['property'].delete_prefix("og:"), elm['content']] }.to_h
     else
       result = {
-        title: document.at_css('meta[name="title"]').try(:[],'content') || document.at_css('title').text
-        description: document.at_css('meta[name="description"]').try(:[],'content')
+        title: document.at_css('meta[name="title"]').try(:[],'content') || document.at_css('title').text,
+        description: document.at_css('meta[name="description"]').try(:[],'content'),
         image: "https://www.google.com/s2/favicons?domain=#{url}&sz=128"
       }
     end
     # image: "https://icons.duckduckgo.com/ip3/#{url}.ico"
     # https://stackoverflow.com/questions/38599939/how-to-get-larger-favicon-from-googles-api/46044485
 
-    document.at_xpath('//head/link[@rel="shortcut icon"]') link rel="shortcut icon"
+    # document.at_xpath('//head/link[@rel="shortcut icon"]') link rel="shortcut icon"
     render json: result
   end
 end
