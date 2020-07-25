e4d6a4dba9d2f784d9fb62ab323b53c59a206cdd
Lucas Kuhn <lucas@nordweg.com>
2020-07-25 18:20:38 -0300

    adds DeleteUser service

---
+++ app/services/delete_user.rb
@@ -0,0 +1,20 @@
+class DeleteUser
+  class << self
+
+    def run(user)
+      # singe delete call
+      user.likes.delete_all
+
+      #destroy_all calls destroy on each object, deleting associated likes
+      user.posts.destroy_all
+
+      user.received_follows.destroy_all
+      user.given_follows.destroy_all
+
+      user.image.purge_now if user.image.attached?
+
+      user.destroy
+    end
+
+  end
+end
