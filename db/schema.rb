601059bc6c8e71c401c93dda70cb6aa108e46ab3
Lucas Kuhn <lucas@nordweg.com>
2020-08-30 11:58:31 -0300

    adds collapsible post, basework for url with metadata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_013447) do
+ActiveRecord::Schema.define(version: 2020_08_28_001316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_013447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.jsonb "url_data"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
